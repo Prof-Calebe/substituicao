eebcb8c0bfd41bd95726406239484dff3248f801
31463738 <31463738@099055-P31S301.macklabs.br>
2016-06-09 09:36:05 -0300

    Criação da Classe de Teste 13, referencia ao CST13 baseado no CSU05.

---
+++ test/servico/cst13Test.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servico;
+
+import datamapper.PopulateDB;
+import datamapper.exceptions.NonexistentEntityException;
+import java.text.ParseException;
+import java.util.LinkedList;
+import java.util.List;
+import modelo.AusenciaModel;
+import modelo.ProfessorModel;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author 31463738
+ */
+public class cst13Test {
+    
+    public cst13Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws NonexistentEntityException, Exception {
+        PopulateDB.recreateDB("prosub", "root", "");
+        PopulateDB.populateUseCaseTest();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() throws ParseException, NonexistentEntityException {
+        //Setup Declaração de Ausência Professor 2
+        ProfessorService professorService = new ProfessorService();
+        ProfessorModel professor = professorService.obterProfessorPorNome("Professor2");
+
+        NotificacaoService notificaçãoService = new NotificacaoService();
+        notificaçãoService.notificarAusencia(professor.id, "25/11/2013 18:30", "25/11/2013 19:59", "Motivo Declarado", new LinkedList<String>());
+
+        //Setup Professor 3 como substituto
+        List<AusenciaModel> ausencias = notificaçãoService.listarAusencias();
+        notificaçãoService.definirSubstituto(ausencias.get(0).codigo, "Professor3");
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testeRejeitarAlocação() throws ParseException
+    {
+        LoginService loginService = new LoginService();
+        assertTrue(loginService.VerificarUsuarioESenha("Professor1", "123456"));
+        
+        NotificacaoService notificaçãoService = new NotificacaoService();
+        List<AusenciaModel> ausencias = notificaçãoService.listarAusenciasPorSubstituto("Professor3");
+        assertEquals(1, ausencias.size());
+        assertEquals("Professor1", ausencias.get(0).professorAusente);
+        assertEquals("Professor2", ausencias.get(0).professorSubstituto);
+        assertEquals("13/06/2016 20:00", ausencias.get(0).dataInicio);
+        assertEquals("13/06/2016 23:00", ausencias.get(0).dataFim);
+        assertEquals("Alocação efetuada", ausencias.get(0).estado); 
+        
+        notificaçãoService.recusarSubstituicao(ausencias.get(0).id);
+        
+        ausencias = notificaçãoService.listarAusencias();
+        assertEquals(1, ausencias.size());
+        assertEquals("Professor1", ausencias.get(0).professorAusente);
+        assertEquals("", ausencias.get(0).professorSubstituto);
+        assertEquals("13/06/2016 20:00", ausencias.get(0).dataInicio);
+        assertEquals("13/06/2016 23:00", ausencias.get(0).dataFim);
+        assertEquals("Alocação pendente", ausencias.get(0).estado);
+    }
+    
+
+    
+}
