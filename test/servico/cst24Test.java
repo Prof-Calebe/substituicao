24d069cd77f2b9ac7e67599f62c4834c0b465eb5
Mohamad <>
2016-06-12 19:48:22 -0300

    Criação da classe cst24Test. Todos os procedimentos do CST24 : Edição de Usuário - Cancela implementados. Código comentado.

---
+++ test/servico/cst24Test.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servico;
+
+import auxiliar.Perfil;
+import datamapper.PopulateDB;
+import datamapper.exceptions.NonexistentEntityException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import modelo.UsuarioModel;
+
+/*
+ *
+ * @author Mohamad
+ *
+ */
+
+public class cst24Test {
+    
+    public cst24Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() throws NonexistentEntityException, Exception{
+        PopulateDB.recreateDB("prosub", "root", "");
+        PopulateDB.populateUseCaseTest();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() throws ParseException, NonexistentEntityException {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testeEdicaoDeUsuarioCancelada() throws ParseException{
+        
+        //Login como usuário "Administrador" 
+        LoginService loginService = new LoginService();
+        assertTrue(loginService.VerificarUsuarioESenha("Administrador", "123456"));
+        
+        //Navegação até "Editar Usuário" 
+        AdministracaoDeUsuariosService editarUs = new AdministracaoDeUsuariosService();
+        List<UsuarioModel> usuarios = editarUs.ListarUsuarios(); 
+        
+        //Seleção e obtenção dos dados do usuário
+        UsuarioModel editarUsuario = usuarios.get(1);
+        assertEquals(editarUsuario.Usuario,"Professor1");
+        assertEquals(editarUsuario.Senha,"123456");
+        
+        try {
+            //Editar usuário
+            editarUs.EditarUsuario("111111", Perfil.PROFESSOR, usuarios.get(1).id);
+        } catch (Exception ex) {
+            Logger.getLogger(cst24Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Verficação de que os dados não foram alterados
+        assertEquals(editarUsuario.Usuario,"Professor1");
+        assertEquals(editarUsuario.Senha, "123456");
+    }
+}
