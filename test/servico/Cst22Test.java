63b37b0e9b796dc2f9e6812935c152f6ae21b930
Victor Messina <>
2016-06-15 22:49:51 -0300

    Modificações realizadas com o objetivo de retirar os erros apontados pelo codacy (package' should be separated from previous statement e  'import' should be separated from previous statement.) e tentativa de remoção do erro apontado pelo travis

--- test/servico/Cst22Test.java
+++ test/servico/Cst22Test.java
@@ -3,15 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servico;
-import auxiliar.Perfil;
-import datamapper.PopulateDB;
-import dominio.Usuario;
-import java.util.List;
-import modelo.UsuarioModel;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+package 
+servico;
+import 
+auxiliar.Perfil;
+import 
+datamapper.PopulateDB;
+import 
+dominio.Usuario;
+import 
+java.util.List;
+import 
+modelo.UsuarioModel;
+import 
+org.junit.BeforeClass;
+import 
+org.junit.Test;
+import static 
+org.junit.Assert.*;
+import 
+org.junit.Before;
 /**
  *
  * @author Victor Messina
@@ -22,6 +33,20 @@ public class Cst22Test {
     public static void setUpClass() throws Exception {
         PopulateDB.recreateDB("prosub", "root", "");
         PopulateDB.populateUseCaseTest();
+    }
+
+    @Before
+    public void setUp() {
+        AdministracaoDeUsuariosService aus;
+
+        aus = new AdministracaoDeUsuariosService();
+        Usuario usuario1 = new Usuario();
+
+        usuario1.setUsuario("Administrador");
+        usuario1.setSenha("123456");
+        usuario1.setPermissao(Perfil.ADMINISTRADOR);
+
+        aus.SalvarUsuario(usuario1.getUsuario(), usuario1.getSenha(), usuario1.getPermissao());
     }
 
     @Test
