29f8c9e301fc88589fc0a17bbbba06154f99b397
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2016-06-22 17:04:54 -0300

    Fixed #19

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ CLASSPATH=$CLASSPATH:$_PWD/lib/ant-contrib-1.0b3.jar:$CopyLibs:$_PWD\glassfish3\
 export CLASSPATH
 echo "CLASSPATH:"$CLASSPATH
 
-wget -nv -T 10 -t 0 "http://dlc.sun.com.edgesuite.net/glassfish/3.1.2/release/glassfish-3.1.2.zip"
+wget -nv -T 10 -t 0 "http://download.java.net/glassfish/3.1.2/release/glassfish-3.1.2.zip"
 # já cria um diretório chamado glassfish3
 unzip -q glassfish-3.1.2.zip
 
