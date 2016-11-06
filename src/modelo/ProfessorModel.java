2a5787af533d07d0896b9134faaf5d4fc3c0174e
Julio Bertolacini <juliolbertolacini@gmail.com>
2016-11-06 17:42:40 -0200

    Fixing "'31' is a magic number." alert (01)

--- src/modelo/ProfessorModel.java
+++ src/modelo/ProfessorModel.java
@@ -46,7 +46,8 @@ public class ProfessorModel {
     @Override
     public int hashCode() {
         int result = 1;
-        result = 31 * result + ( (this.Nome == null) ? 0 : this.Nome.hashCode() );
+        final int HASH_MULTIPLIER = 31; 
+        result = HASH_MULTIPLIER * result + ( (this.Nome == null) ? 0 : this.Nome.hashCode() );
         return result;
     }
     
