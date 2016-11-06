3ded2e6dc26f33fef5207cb4561f292bf10c54e2
Julio Bertolacini <juliolbertolacini@gmail.com>
2016-11-06 18:29:19 -0200

    Fixing "Missing package-info.java file." alert (02). Also "'package' should be separated from previous statement."

--- src/modelo/ProfessorModel.java
+++ src/modelo/ProfessorModel.java
@@ -1,11 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @since 1.0
+ * @author Rick & Julio Bertolacini
+ * @version 1.0
  */
 
-/**
- * Package: modelo
- */
 package modelo;
 
 /**
