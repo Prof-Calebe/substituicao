d450906e5bef4f409a68e5ae11c315032dd8eb4e
Leonardo Fuso <Leonardo Fuso>
2016-11-18 10:50:31 -0200

    Correção de erros gerados na Refatoração de NotificacaoService

--- src/servico/NotificacaoService.java
+++ src/servico/NotificacaoService.java
@@ -27,25 +27,25 @@ import org.joda.time.Interval;
  * @author Thiago Lima
  */
 public class NotificacaoService {
-    
+
     private final AusenciaJpaController ausenciaController;
 
     private final ProfessorJpaController profController;
-    
-    public NotificacaoService(){
+
+    public NotificacaoService() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pro_subPU");
         ausenciaController = new AusenciaJpaController(emf);
 //        periodoController = new PeriodoJpaController(emf);
         profController = new ProfessorJpaController(emf);
     }
-    
+
     public String notificarAusencia(Long idProfessor, String dataInicio, String dataFim, String motivo, List<String> nomesProfessoresIndicados) throws ParseException {
-        
+
         SimpleDateFormat sdf = null;
-        
+
         DateTime inicio = null;
         DateTime fim = null;
-        
+
         try {
             sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
             inicio = new DateTime(sdf.parse(dataInicio));
@@ -57,129 +57,127 @@ public class NotificacaoService {
         }
 
         Interval periodo = new Interval(inicio, fim);
-        
+
         Professor professor = profController.findProfessor(idProfessor);
-        
+
         Random r = new Random();
-               
+
         List<Ausencia> ausencias = professor.gerarAusencias(periodo, motivo);
-        
+
         List<String> codigos = new LinkedList<>();
-        
-        for(Ausencia ausencia : ausencias)
-        {
+
+        for (Ausencia ausencia : ausencias) {
             String codigo = Integer.toString(r.nextInt(10000));
-            
+
             ausencia.setCodigo(codigo);
-            
+
             codigos.add(codigo);
-            
-            for(String nomeProf : nomesProfessoresIndicados){
+
+            for (String nomeProf : nomesProfessoresIndicados) {
                 Professor professorIndicado = profController.findProfessor(nomeProf);
                 ausencia.indicarSubstituto(professorIndicado);
             }
 
             ausenciaController.create(ausencia);
         }
-        
-        if(codigos.size() > 0)
-            return codigos.get(0);       
-        else
+
+        if (codigos.size() > 0) {
+            return codigos.get(0);
+        } else {
             return "0";
+        }
     }
 
     public List<Ausencia> listarAusencias() {
-        
+
         List<Ausencia> ausencias = ausenciaController.findAusenciaEntities();
         List<Ausencia> modelos = new LinkedList<>();
-        
-        for(Ausencia ausencia : ausencias){
-            
+
+        for (Ausencia ausencia : ausencias) {
+
             Ausencia modelo = this.montarAusencia(ausencia);
-            
+
             modelos.add(modelo);
         }
-        
+
         return modelos;
     }
-    
+
     /*
-    public List<Ausencia> listarAusenciasPorEstado(EstadoAusencia estado){
+     public List<Ausencia> listarAusenciasPorEstado(EstadoAusencia estado){
         
-        List<Ausencia> ausencias = ausenciaController.findAusenciaEntities();
-        List<Ausencia> modelos = new ArrayList<Ausencia>();
+     List<Ausencia> ausencias = ausenciaController.findAusenciaEntities();
+     List<Ausencia> modelos = new ArrayList<Ausencia>();
         
-        for(Ausencia ausencia : ausencias){
+     for(Ausencia ausencia : ausencias){
             
-            if(ausencia.getEstado().equals(estado)){
-                Ausencia modelo = this.montarAusencia(ausencia);
+     if(ausencia.getEstado().equals(estado)){
+     Ausencia modelo = this.montarAusencia(ausencia);
                 
-                modelos.add(modelo);   
-            }               
-        }            
-        return modelos;   
-    }
-    */
-    
-    public List<Ausencia> listarAusenciasPorProfessor(String usernameProfessor){
-        
+     modelos.add(modelo);   
+     }               
+     }            
+     return modelos;   
+     }
+     */
+    public List<Ausencia> listarAusenciasPorProfessor(String usernameProfessor) {
+
         Professor professor = profController.findProfessorPorUsername(usernameProfessor);
-        
+
         List<Ausencia> ausenciasPorProfessor = ausenciaController.listAusenciasPorProfessor(professor);
-        
+
         List<Ausencia> modelos = new ArrayList<>();
-        
-        for(Ausencia ausencia : ausenciasPorProfessor){
-            
+
+        for (Ausencia ausencia : ausenciasPorProfessor) {
+
             Ausencia modelo = this.montarAusencia(ausencia);
-            
+
             modelos.add(modelo);
-            
+
         }
-        
+
         return modelos;
     }
-    
-    public List<Ausencia> listarAusenciasPorIndicacaoDeSubstituto(String usernameProfessor){
-        
+
+    public List<Ausencia> listarAusenciasPorIndicacaoDeSubstituto(String usernameProfessor) {
+
         Professor professorIndicado = profController.findProfessorPorUsername(usernameProfessor);
-        
+
         List<Ausencia> ausenciasComIndicacaoDeSubstituto = ausenciaController.listAusenciasPorIndicacaoDeSubstituto(professorIndicado);
-        
+
         List<Ausencia> modelos = new ArrayList<>();
-        
-        for(Ausencia ausencia : ausenciasComIndicacaoDeSubstituto){
-            
+
+        for (Ausencia ausencia : ausenciasComIndicacaoDeSubstituto) {
+
             Ausencia modelo = this.montarAusencia(ausencia);
-            
+
             modelos.add(modelo);
-            
+
         }
-        
+
         return modelos;
     }
-    
-    public List<Ausencia> listarAusenciasPorSubstituto(String usernameProfessor){
-        
+
+    public List<Ausencia> listarAusenciasPorSubstituto(String usernameProfessor) {
+
         Professor professorIndicado = profController.findProfessorPorUsername(usernameProfessor);
-        
+
         List<Ausencia> ausenciasComIndicacaoDeSubstituto = ausenciaController.listAusenciasPorSubstituto(professorIndicado);
-        
+
         List<Ausencia> modelos = new ArrayList<>();
-        
-        for(Ausencia ausencia : ausenciasComIndicacaoDeSubstituto){
-            
+
+        for (Ausencia ausencia : ausenciasComIndicacaoDeSubstituto) {
+
             Ausencia modelo = this.montarAusencia(ausencia);
-            
+
             modelos.add(modelo);
-            
+
         }
-        
+
         return modelos;
     }
-    
-    public void aceitarSubstituicao(Long ausenciaId)
-    {        
+
+    public void aceitarSubstituicao(Long ausenciaId) {
         try {
             Ausencia ausencia = ausenciaController.findAusencia(ausenciaId);
             ausencia.confirmar();
@@ -190,9 +188,8 @@ public class NotificacaoService {
             Logger.getLogger(NotificacaoService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void recusarSubstituicao(Long ausenciaId)
-    {
+
+    public void recusarSubstituicao(Long ausenciaId) {
         try {
             Ausencia ausencia = ausenciaController.findAusencia(ausenciaId);
             ausencia.recusar();
@@ -203,52 +200,61 @@ public class NotificacaoService {
             Logger.getLogger(NotificacaoService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private Ausencia montarAusencia(Ausencia ausencia){
+
+    private Ausencia montarAusencia(Ausencia ausencia) {
 
         Ausencia modelo = new Ausencia();
 
         modelo.setCodigo(ausencia.getCodigo());
         modelo.setProfessor(ausencia.getProfessor());
-        
-        if(ausencia.getProfessorSubstituto() != null){
-            modelo.setProfessorSubstituto(ausencia.getProfessorSubstituto());    
-        }else{
+
+        if (ausencia.getProfessorSubstituto() != null) {
+            modelo.setProfessorSubstituto(ausencia.getProfessorSubstituto());
+        } else {
             Professor professor = new Professor("", "");
             modelo.setProfessorSubstituto(professor);
         }
-        
+
         //modelo.professorSubstituto = ausencia.getIndicacoesSubstitutos().getNome();
         modelo.setEstado(ausencia.getEstado());
         modelo.setId(ausencia.getId());
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        
+
         Interval periodo = ausencia.getPeriodo();
-        
-        modelo.setPeriodo(periodo);
-        
-        
+
+        DateTime inicio;
+        DateTime fim;
+        String dataInicio = sdf.format(periodo.getStart().toDate());
+        String dataFim = sdf.format(periodo.getEnd().toDate());
+
+        inicio = new DateTime((dataInicio));
+        fim = new DateTime((dataFim));
+
+        Interval periodoModel = new Interval(inicio, fim);
+
+        modelo.setPeriodo(periodoModel);
+
         return modelo;
-        
+
     }
 
     public void definirSubstituto(String codigo, String nomeProfessor) {
-        
+
         Professor profSubstituto = profController.findProfessor(nomeProfessor);
 
-        if(profSubstituto == null){
+        if (profSubstituto == null) {
             throw new IllegalStateException("Professor de nome " + nomeProfessor + " não existe");
         }
-        
+
         Ausencia ausencia = ausenciaController.findAusencia(codigo);
-        
-        
-        if(ausencia == null){
+
+        if (ausencia == null) {
             throw new IllegalStateException("Ausência de código " + codigo + " não existe");
-        }        
-        
+        }
+
         ausencia.setProfessorSubstituto(profSubstituto);
-        
+
         try {
             ausenciaController.edit(ausencia);
         } catch (NonexistentEntityException ex) {
@@ -257,13 +263,12 @@ public class NotificacaoService {
             Logger.getLogger(NotificacaoService.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        
     }
-    
-    public void cancelarAusencia(String codigo){
-        
+
+    public void cancelarAusencia(String codigo) {
+
         Ausencia ausencia = ausenciaController.findAusencia(codigo);
-        
+
         ausencia.cancelarAusencia();
         try {
             ausenciaController.edit(ausencia);
@@ -272,13 +277,13 @@ public class NotificacaoService {
         } catch (Exception ex) {
             Logger.getLogger(NotificacaoService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
-    public void cancelarAulas(String codigo){
-        
+
+    public void cancelarAulas(String codigo) {
+
         Ausencia ausencia = ausenciaController.findAusencia(codigo);
-        
+
         ausencia.cancelarAulas();
         try {
             ausenciaController.edit(ausencia);
@@ -287,7 +292,7 @@ public class NotificacaoService {
         } catch (Exception ex) {
             Logger.getLogger(NotificacaoService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
 }
